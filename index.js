8c69a2e107301e8026f2068d998bc4e465fd4c39
Gerard Compion <gcompion93@gmail.com>
2022-01-21 16:49:49 -0500

    added Account class and updated transaction objects to interact with accounts

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+}
 
 class Withdrawal {
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;
   }
 }
 
 class Deposit {
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance += this.amount;
+    this.account.balance += this.amount;
   }
 }
 
@@ -25,17 +32,18 @@ class Deposit {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account('snow-patrol');
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1, '\n----');
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2, '\n----');
 
-t3 = new Deposit(120.00);
+t3 = new Deposit(120.00, myAccount);
 t3.commit();
 console.log('Transaction 3:', t3, '\n----');
 
-console.log('Balance:', balance, '\n----');
+console.log('Balance:', myAccount.balance, '\n----');
