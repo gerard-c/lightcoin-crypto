4bae21417183e0ba75a56f39b4bea61005c76bd4
Gerard Compion <gcompion93@gmail.com>
2022-01-21 17:21:48 -0500

    added functionality to keep track of transactions and verify if account has enough money for withdrawals

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 class Account {
   constructor(username) {
     this.username = username;
-    this.balance = 0;
+    this.transactions = [];
+  }
+
+  get balance() {
+    let balance = 0;
+    for (const transaction of this.transactions) {
+      balance += transaction.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 }
 
@@ -10,8 +22,12 @@ class Transaction {
     this.amount = amount;
     this.account = account;
   }
+
   commit() {
-    this.account.balance += this.value;
+    if (!this.isAllowed()) return false;
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
   }
 }
 
@@ -19,11 +35,19 @@ class Withdrawal extends Transaction {
   get value() {
     return -this.amount;
   }
+
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
 }
 
 class Deposit extends Transaction{
   get value () {
     return this.amount;
+  }
+
+  isAllowed() {
+    return true;
   }
 }
 
@@ -45,5 +69,17 @@ console.log('Transaction 2:', t2, '\n----');
 t3 = new Deposit(120.00, myAccount);
 t3.commit();
 console.log('Transaction 3:', t3, '\n----');
+
+t4 = new Withdrawal(59.76, myAccount);
+t4.commit();
+console.log('Transaction 4:', t4, '\n----');
+
+t5 = new Deposit(679.56, myAccount);
+t5.commit();
+console.log('Transaction 5:', t5, '\n----');
+
+t6 = new Withdrawal(680, myAccount);
+t6.commit();
+console.log('Transaction 6:', t6, '\n----');
 
 console.log('Balance:', myAccount.balance, '\n----');
